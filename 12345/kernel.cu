c120fb140bd8a272890615bdb0ebb000a7f3f933
basarab1504 <45500276+basarab1504@users.noreply.github.com>
2020-10-28 15:39:59 +0300

    time measuring added

--- 12345/kernel.cu
+++ 12345/kernel.cu
@@ -1,42 +1,76 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
-
+#include <ctime>
 #include <stdio.h>
 
 __global__ void squareKernel(int* data, int N);
 
 int main(int argc, char** argv)
 {
+	int* arr;
 	int* h_data;
 	int* d_data;
-	//количество квадратов
-	int n = 1000;
+	//количество квадратов + 1
+	int n = 256;
 	//сумма квадратов
 	int sum = 0;
+	int sumCPU = 0;
+
+	//эвенты для подсчета времени выполнения на GPU
+	cudaEvent_t g_start, g_stop;
+	cudaEventCreate(&g_start);
+	cudaEventCreate(&g_stop);
+	//clock для подсчета времени выполнения на CPU
+	clock_t c_start, c_stop;
 
 	// выделяем page-locked память на хосте
 	// эту функцию лучше всего использовать экономно для выделения промежуточных областей для обмена данными между хостом и устройством.
 	cudaHostAlloc(&h_data, n * sizeof(int), cudaHostAllocPortable);
 
+	//cudaMemcpy(h_data, arr, n * sizeof(int), cudaMemcpyHostToDevice);
+
 	// выделяем память на устройстве
 	cudaMalloc(&d_data, n * sizeof(int));
-	
+
 	dim3 block(512);
 	dim3 grid((n + block.x - 1) / block.x);
 
 	//grid - количество блоков
 	//block - размер блока
-	squareKernel<<<grid, block>>>(d_data, n);
+	squareKernel <<<grid, block>>>(d_data, n);
+
+	cudaEventRecord(g_start, 0);
 
 	//копируем данные с устройства (d_data) на хост (h_data)
 	cudaMemcpy(h_data, d_data, n * sizeof(int), cudaMemcpyDeviceToHost);
+
+	cudaEventRecord(g_stop, 0);
+	cudaEventSynchronize(g_stop);
+	float GPUelapsedTime;
+	cudaEventElapsedTime(&GPUelapsedTime, g_start, g_stop);
 
 	for (int j = 0; j < n; j++)
 	{
 		sum = sum + h_data[j];
 	}
 
-	printf("sum = %d\n", sum);
+	//CPU
+	double CPUelapsedTime;
+	c_start = clock();
+	for (int i = 0; i < n; i++)
+	{
+		sumCPU += i * i;
+	}
+	c_stop = clock();
+	CPUelapsedTime = ((double)(c_stop - c_start)) / (CLOCKS_PER_SEC * 1000);
+
+	printf("Time in GPU: %lfms\n", GPUelapsedTime);
+	printf("Time in CPU: %lfms\n", CPUelapsedTime);
+	printf("sum GPU for %d = %d\n", n-1, sum);
+	printf("sum CPU for %d = %d\n", n-1, sumCPU);
+
+	cudaEventDestroy(g_start);
+	cudaEventDestroy(g_stop);
 	return 0;
 }
 
